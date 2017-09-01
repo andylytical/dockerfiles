78dc8a7c2cebb1613303210fda0b3b08bd8fcb41
Andy Loftus <aloftus@gmail.com>
2017-09-01 14:33:15 -0500

    Remove image name

--- Python3/doit.ps1
+++ Python3/doit.ps1
@@ -1,11 +1,10 @@
 # Windows PowerShell
 
-$image_name = "py-sql-dev"
-$container_name = "py3dev"
+$image_name = "py3dev"
 $homedir = "J:\aloftus"
 
 # BUILD IMAGE
 docker build . -t $image_name
 
 # RUN IMAGE IN CONTAINER
-docker run --rm -it --name $container_name --mount type=bind,src=$homedir,dst=/home/aloftus $image_name
+docker run --rm -it --mount type=bind,src=$homedir,dst=/home/aloftus $image_name
